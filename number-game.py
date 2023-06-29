8a029360d3aaf8d634c8c7588a558fbdc273eddb
Oguz Turhan <50463150+oguzturhan@users.noreply.github.com>
2023-06-29 00:51:22 -0400

    Update number-game.py

--- number-game.py
+++ number-game.py
@@ -10,18 +10,18 @@ while cont == 'y':
     live = int(input('How many lives do you want? :'))
     print('Now.. Guess the number in my mind.  It is between 0-100')
     number = random.randint(0, 100)
-    kalan = live
+    counter = live
     for a in range(0,live):
-        print(f'Lives :{kalan}')
-        tahmin = int(input('Guess :'))
-        kalan = kalan - 1
-        if tahmin == number:
-            print(f'Congrats! Win!!! Correct number is: {tahmin}')
+        print(f'Lives :{counter}')
+        guess = int(input('Guess :'))
+        counter =- 1
+        if guess == number:
+            print(f'Congrats! Win!!! Correct number is: {guess}')
             break
-        elif tahmin > number and kalan > 0:
+        elif guess > number and counter > 0:
             os.system('cls')
             print('Enter smaller number!')
-        elif tahmin < number and kalan > 0:
+        elif guess < number and counter > 0:
             os.system('cls')
             print('Enter bigger number!')
         else:
